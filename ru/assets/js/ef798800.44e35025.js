88870dac9b66cef97134ec70cc03fb2dcaede681
Grigoriy Zaripov <user.name>
2022-10-12 23:53:24 +0300

    Deploy website - based on b1e04dc961ea7d225e1a7c531d29a9f2407a0073

--- ru/assets/js/ef798800.44e35025.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[2534],{3143:e=>{e.exports=JSON.parse('{"permalink":"/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);

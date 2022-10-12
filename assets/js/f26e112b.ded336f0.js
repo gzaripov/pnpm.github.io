5dc583b60abd30fa1d4ee081d653ad994e86a82a
Grigoriy Zaripov <user.name>
2022-10-13 00:00:17 +0300

    Deploy website - based on 7f747d9044c37568673f81085a1927ca52755f62

---
+++ assets/js/f26e112b.ded336f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[2411],{2538:e=>{e.exports=JSON.parse('{"permalink":"/pnpm.github.io/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ ru/assets/js/0bff62da.3c41a56b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[1847],{8247:e=>{e.exports=JSON.parse('{"permalink":"/pnpm.github.io/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
